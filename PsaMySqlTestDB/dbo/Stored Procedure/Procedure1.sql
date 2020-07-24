152b18dd667953ddd4d2fb8c0f926293d41f9291
Tommy <TheTommyHsieh@gmail.com>
2020-07-24 16:17:47 +0800

    [^]展開萬用字元。

--- PsaMySqlTestDB/dbo/Stored Procedure/Procedure1.sql
+++ PsaMySqlTestDB/dbo/Stored Procedure/Procedure1.sql
@@ -2,7 +2,8 @@ CREATE PROCEDURE [dbo].[Procedure1]
 	@id int
 AS
 begin
-	SELECT *
+	SELECT [id], [part_number], [apply_station], 
+		[distend_X], [distend_Y], [eco]
 	from [dbo].[film_entry]
 	where id = @id;
 end
